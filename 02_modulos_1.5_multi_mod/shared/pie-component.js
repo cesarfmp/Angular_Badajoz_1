051e427473f125bc089d50987c97ac342263846f
cesarfmp <32636313+cesarfmp@users.noreply.github.com>
2017-10-10 10:23:26 +0200

    Estructurar los modulos

---
+++ 02_modulos_1.5_multi_mod/shared/pie-component.js
@@ -0,0 +1,26 @@
+class PieController {
+    
+    // Inyeccion de dependecias
+    constructor() {}
+
+    // Es el constructor, para evitar los problemas de instaciacion
+    $onInit() {
+        this.sCurso = "Angular JS";
+    };    
+
+   
+    //$onChanges (changesObj) { };
+    //$onDestroy() { };
+}
+
+// Nos creamos un componente para la cabecera que va a pertenecer al modulo shared
+// Para hacer referencia a este componente usamos app-cabeza por llamarse AppCabeza
+// Podemos usar la plantilla directamente en vez de la url
+angular.module('sharedModule')
+        .component('appPie', {
+            template:`Curso de {{$ctrl.sCurso}}`,
+            controller: PieController,
+            //controllerAs: '$ctrl',  --> por defecto usar $ctrl
+        });
+
+    
