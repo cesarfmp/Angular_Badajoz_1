ce5d5a38ea10355e69eda9aa94a74be8d84f7dee
cesarfmp <32636313+cesarfmp@users.noreply.github.com>
2017-10-11 10:06:10 +0200

    storage

--- 08_Servicio_Locasl_Storage/main/main-component.js
+++
@@ -1,9 +0,0 @@
-class MainController {
-    $onInit() {}
-}
-
-angular.module("appModule")
-.component('appMain', {
-    templateUrl: './main/main-partial.html',
-    controller: MainController,
-});
